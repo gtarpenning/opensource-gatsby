e37edbdf3aafd1915208f45fd7f94789fba0769d
gtarpenning <gtarpenning@gmail.com>
2021-12-04 17:17:37 -0800

    major overhaul, now new components use jsx template and are ~sexy~

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Open-source`,
+    siteHeadline: `Open Source`,
+    siteUrl: `https://opensourcegatsby.gatsbyjs.io/`,
+    siteDescription: `CS208e Project on the Open Source Movement`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `griffin`,
   },
   plugins: [
     {
---
+++ src/pages/stallman.jsx
@@ -0,0 +1,74 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { Parallax } from "@react-spring/parallax"
+import { Themed } from "theme-ui"
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
+import Divider from "@lekoarts/gatsby-theme-cara/src/elements/divider"
+import { UpDown, UpDownWide } from "@lekoarts/gatsby-theme-cara/src/styles/animations"
+import Svg from "@lekoarts/gatsby-theme-cara/src/components/svg"
+import Seo from "@lekoarts/gatsby-theme-cara/src/components/seo"
+import Content from "@lekoarts/gatsby-theme-cara/src/elements/content"
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
+
+const Stallman = () => (
+  <Layout>
+    <Parallax pages={2}>
+      <div>
+        <Divider speed={0.2} offset={0} factor={1}>
+          <UpDown>
+            <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
+            <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
+            <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" />
+          </UpDown>
+          <UpDownWide>
+            <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
+            <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+            <Svg icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
+            <Svg icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
+            <Svg icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
+            <Svg icon="circle" width={6} color="icon_darkest" left="75%" top="10%" />
+            <Svg icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+          </UpDownWide>
+          <Svg icon="circle" hiddenMobile width={24} color="icon_darker" left="5%" top="70%" />
+          <Svg icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
+          <Svg icon="circle" width={12} color="icon_darkest" left="50%" top="60%" />
+          <Svg icon="upDown" width={8} color="icon_darkest" left="95%" top="90%" />
+          <Svg icon="upDown" hiddenMobile width={24} color="icon_darker" left="40%" top="80%" />
+          <Svg icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" />
+          <Svg icon="circle" width={64} color="icon_green" left="95%" top="5%" />
+          <Svg icon="box" hiddenMobile width={64} color="icon_purple" left="5%" top="90%" />
+          <Svg icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
+          <Svg icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
+          <Svg icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
+          <Svg icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
+        </Divider>
+        <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0.13} factor={1}>
+          <Inner>
+            <Themed.h1>Richard Stallman</Themed.h1>
+            <Themed.p>
+              <img src='https://media.wired.com/photos/5d815ffe46103c0009de8d56/master/w_960,c_limit/science_stallman_473688628.jpg' />
+            </Themed.p>
+            <Themed.h2>Background</Themed.h2>
+            <Themed.p>
+              1. Worked at AT&T Bell Labs, UNIX, MIT’s AI Laboratory
+            </Themed.p>
+            <Themed.p>
+              2. Quit to begin free-software development
+            </Themed.p>
+            <Themed.p>
+              3. Creation of GPL license, or “copyleft”
+            </Themed.p>
+            <Themed.p>
+              4. No cell phone, no Facebook/Twitter etc
+            </Themed.p>
+            <Themed.p>
+
+            </Themed.p>
+          </Inner>
+        </Content>
+      </div>
+    </Parallax>
+  </Layout>
+)
+
+export default Stallman

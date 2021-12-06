7b7fe6be40f70f5c1ac97262a7ad8d59d17e8f65
gtarpenning <gtarpenning@gmail.com>
2021-12-06 14:01:56 -0800

    major content additions

--- src/pages/impact.jsx
+++ src/pages/impact.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import { Parallax } from "@react-spring/parallax"
 import { Themed } from "theme-ui"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
@@ -11,9 +11,9 @@ import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
 
 const Impact = () => (
     <Layout>
-      <Parallax pages={2}>
+      <Parallax pages={3}>
         <div>
-          <Divider speed={0.2} offset={0} factor={1}>
+          <Divider speed={0.2} offset={0.1} factor={2.5}>
             <UpDown>
               <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
               <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
@@ -41,11 +41,11 @@ const Impact = () => (
             <Svg icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
             <Svg icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
           </Divider>
-          <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0.13} factor={1}>
+          <Content sx={{ variant: `texts.bigger` }} speed={0.2} offset={0.2} factor={2}>
             <Inner>
               <Themed.h1>The Impact of Open Source</Themed.h1>
-              <Themed.p>
-                <img src='https://i1.rgstatic.net/publication/313891500_Impact_of_Financial_Benefits_on_Open_Source_Software_Sustainability/links/5a667df6aca2720ba6af5017/largepreview.png' width="340" height="440" />
+              <Themed.p syle={{backgroundColor: 'white'}}>
+              <img alt='silly' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfT2r2Sre8n6y2NLFqpDvr04fVD-v3hr0wJKEMa2wgZfvWX82q3Tldik6loyKEw5pAaXk&usqp=CAU' width="471" height="370" />
               </Themed.p>
               <Themed.p>
               - European Commission: “It is estimated that companies located in the EU invested around €1 billion in Open Source Software in 2018, which brought about a positive impact on the European economy of between €65 and €95 billion.”
@@ -57,6 +57,33 @@ const Impact = () => (
               - As part of that same study, they also found 64 percent of companies currently participate in open source projects              </Themed.p>
               <Themed.p>
               - From  Red Hat’s new 2020 State of Enterprise Open Source Report: 83 percent said that enterprise open source is instrumental to their organizations’ abilities to take advantage of cloud architectures.
+              </Themed.p>
+              <Themed.h3>
+                RedHat
+              </Themed.h3>
+              <Themed.p>
+              Mark Ewing was a technologist that, in the early 90’s would spin off his own versions of the up-and-coming OS Linux. He would sell CD with the software to small businesses, creating an alternative to Microsoft. Bob Young noticed the demand and chose to team up with Mark Ewing to incorporate Red Hat in 1995. Red Hat offered a “constantly evolving community developed operating system…”. 
+              </Themed.p>
+              <Themed.p>
+              “Red Hat makes, maintains, and contributes to multiple free software projects, which shows its open-source spirit. It has bought many companies with proprietary software product codebases and released the software under open source licenses. As of March 2016, Red Hat is the second-largest corporate contributor to the Linux kernel version 4.14 after Intel.”
+              </Themed.p>
+              <Themed.p>
+              RedHat is now a subsidiary of of IBM
+              </Themed.p>
+              <Themed.h3>
+              Cloudera
+              </Themed.h3>
+              <Themed.p>
+              Cloudera is another open-source company that was founded in 2008. However, it was part of the second generation of open-source companies. This new generation had two differences: software was principally developed in an existing company (Apache Hadoop within Yahoo) and only parts of the software were licenced off for free.              
+              </Themed.p>
+              <Themed.p>
+              The founding of Cloudera changed the economic model of open-source business. Individual companies no longer had “moral authority” over the software, which allowed other competitors to offer more and more parts of their software for free.
+              </Themed.p>
+              <Themed.h4>
+              “Cloudera is an enterprise software company that delivers a modern data management and analytics platform built on Apache Hadoop and the latest open source technologies.mbers here” -Ed Nair, Dataquest
+              </Themed.h4>
+              <Themed.p>
+              Such changes would bring in a new generation of companies: SaaS
               </Themed.p>
             </Inner>
           </Content>
--- src/pages/stallman.jsx
+++ src/pages/stallman.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Parallax } from "@react-spring/parallax"
 import { Themed } from "theme-ui"
+import { Link } from "gatsby"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
 import Divider from "@lekoarts/gatsby-theme-cara/src/elements/divider"
 import { UpDown, UpDownWide } from "@lekoarts/gatsby-theme-cara/src/styles/animations"
@@ -10,28 +11,22 @@ import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
 
 
 const Stallman = () => {
-  const MAX_ARTICLES = 10;
-  const [articles, setArticles] = useState();
-
-  useEffect(() => {
-    const loadArticles = async () => {
-      fetch('https://stallman.org/rss/rss.xml', { headers: { Accept: "application/json" } })
-      .then((res) => res.json())
-      .then((data) => data.items.filter((item) => item.title.length > 0))
-      .then((newArticles) => newArticles.slice(0, MAX_ARTICLES))
-      .then((articles) => setArticles(articles))
-      .catch((error) => console.log(error));
-      };
-    loadArticles();
-  }, [MAX_ARTICLES])
-
-  console.log(articles)
+  const lines = [
+    "Harvard Physics --> MIT Lab AI",
+    "No cell phone, no Facebook/Twitter etc.",
+    "Worked at AT&T, UNIX",
+    "GNU Manifesto",
+    "Linux, Emacs, etc. @ the Free Software Foundation (founded in 1985).",
+    "Bell Labs Creation of GPL license, or “copyleft”",
+    "Recent Controversy -- Epstein Comments",
+    "Amazing website!",
+  ]
   
   return (
   <Layout>
-    <Parallax pages={2}>
+    <Parallax pages={3}>
       <div>
-        <Divider speed={0.2} offset={0} factor={1}>
+        <Divider speed={0.2} offset={0.1} factor={2}>
           <UpDown>
             <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
             <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
@@ -59,34 +54,25 @@ const Stallman = () => {
           <Svg icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
           <Svg icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
         </Divider>
-        <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0.13} factor={1}>
+        <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0.33} factor={1}>
           <Inner>
-            <Themed.h1>Richard Stallman</Themed.h1>
+            <Themed.h1>Richard Stallman <Link to="https://stallman.org/">[link]</Link></Themed.h1>
             <Themed.p>
               <img alt='stall' src='https://media.wired.com/photos/5d815ffe46103c0009de8d56/master/w_960,c_limit/science_stallman_473688628.jpg' />
             </Themed.p>
             <Themed.h2>Background</Themed.h2>
-            <Themed.p>
-              1. Worked at AT&T Bell Labs, UNIX, MIT’s AI Laboratory
-            </Themed.p>
-            <Themed.p>
-              2. Quit to begin free-software development
-            </Themed.p>
-            <Themed.p>
-              3. Creation of GPL license, or “copyleft”
-            </Themed.p>
-            <Themed.p>
-              4. No cell phone, no Facebook/Twitter etc
-            </Themed.p>
-            <Themed.p>
-
-            </Themed.p>
+            {lines.map(x => (
+              <Themed.p key={x}> 
+                {x} 
+              </Themed.p>
+            ))
+            }
             <Themed.h2>
               Official Stallman RSS Feed:
             </Themed.h2>
             </Inner>
           </Content>
-          <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={.85} factor={2} backgroundColor='gray'>
+          <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={1} factor={2} backgroundColor='gray'>
             <iframe height="1100" width="75%" frameBorder="0" scrolling="auto" title="lol"
                     style={{background: "lightgray", borderRadius: "25px", padding: "10px"}}
                     src="https://stallman.org/iframe-feed.html">If</iframe>
